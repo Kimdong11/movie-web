ee1b66597cfcc752fed6e08dc675537b429b7226
김동현 <kdh@gimdonghyeon-ui-MacBookAir.local>
2022-02-27 04:42:58 +0900

    Rewrite README.md

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Movie from '../components/Movie';
+import styles from './Home.module.css';
 
 const Home = () => {
     const [loading, setLoading] = useState(true);
@@ -13,16 +14,19 @@ const Home = () => {
         getMovies();
     }, []);
     return (
-        <div>
+        <div className={styles.container}>
             {loading ? (
-                <h1>Loading...</h1>
+                <div className={styles.loader}>
+                    <span>Loading...</span>
+                </div>
             ) : (
-                <div>
+                <div className={styles.movies}>
                     {movies.map((movie) => {
                         return (
                             <Movie
                                 key={movie.id}
                                 id={movie.id}
+                                year={movie.year}
                                 coverImg={movie.medium_cover_image}
                                 title={movie.title}
                                 summary={movie.summary}
